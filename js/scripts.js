d260c5da0e411d5754d866b10ff58b552a9bbaaa
Łukasz Wasiluk <galawanty@gmail.com>
2017-03-06 21:51:56 +0100

    zadanie 7

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var txt = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+var animalUpperCased = animal.toUpperCase('Zielone słonie');
+var animal = txt.replace('Papugi', animalUpperCased);
+console.log(animal.substr(0, 45));
